a405c9bd1a28a77f083f23f19e885a332f4aecb9
Gia Bao <hieutiti1992103@gmail.com>
2023-10-06 18:49:56 +0700

    abc

---
+++ attribute/attribute/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+using System.Reflection.Metadata.Ecma335;
+
+namespace attribute //vi du thao tac len thuoc tinh
+{
+    class TestResult
+    {
+        public readonly string ID;
+
+        private string Name1;
+        public string Name
+        {
+            get { return Name1; }
+            set { Name1 = value; }
+        }
+        private string Address1;
+        public string Address
+        {
+            get { return Address1; }
+            set { Address1 = value; }
+        }
+        private int Age1;
+        public int Age
+        {
+            get { return Age1; }
+            //rang buoc gia tri cho thuoc tinh
+            set
+            {
+                if (value > 100)
+                    Age1 = 100;
+                else
+                    if (value <= 0)
+                    Age1 = 20;
+                else Age1 = value;
+            }
+        }
+        private double Grade1;
+        public double Grade
+        {
+            get { return Grade1; }
+            // ràng buộc giá trị cho thuộc tính
+            set
+            {
+                if (value > 10)
+                    Grade1 = 10;
+                else if (value < 0)
+                    Grade1 = 0;
+                else Grade1 = value;
+            }
+
+        }
+        //khai bao phuong thucc
+        public string Result()
+        {
+            if (Grade < 5)
+                return "Rot";
+            else return "Dau";
+        }
+        private string Password1;
+        // khai bao thuoc tinh Password chi ghi
+        public string Password //chi ghi,khong duoc truy xuat
+        {
+            set { Password1 = value; }
+        }
+        private string DefaultPass1;
+        public string DefaultPass
+        {
+            get { return DefaultPass1; }
+        }
+        public TestResult()
+        {
+            Console.Write("Nhap ID: ");
+            ID = Console.ReadLine();
+            DefaultPass1 = "pass@OOP";
+        }
+        static void Main(string[] args)
+        {
+            TestResult res1 = new TestResult();
+            Console.Write("Nhap ten: ");
+            res1.Name = Console.ReadLine();
+            Console.Write("Nhap dia chi: ");
+            res1.Address = Console.ReadLine();
+            Console.Write("Nhap tuoi: ");
+            res1.Age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Nhap diem: ");
+            res1.Grade = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Nhap password moi: ");
+            res1.Password = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("----------Ket qua-----------");
+            Console.WriteLine("ID = {0} , Name= {1}; \n Address= {2};", res1.ID, res1.Name, res1.Address);
+            Console.WriteLine("Age= {0}; \n Grade= {1}; \n Result = {2}; \n DefaultPass= {3} ", res1.Age, res1.Grade, res1.Result(), res1.DefaultPass);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ bai tap chuong 1/bai tap chuong 1/Program.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace baihieu
+{
+    class Program
+    {
+
+        static double bai1(int n)//S=1+1/2 +1/3 + 1/n
+        {
+            int i;
+            double S = 0;
+            for (i = 1; i <= n; i++)
+                S = S + (1.0) / i;
+            return S;
+        }
+        static double bai2(int n) //S=1^3 + 2^3 + n^3
+        {
+            int i;
+            double S = 0;
+            for (i = 1; i <= n; i++)
+                S = S + Math.Pow(i, 3);
+            return S;
+        }
+        static double bai3(int x, int n) // T = x^n
+        {
+            double T = 1;
+            for (int i = 1; i <= n; i++)
+                T = T * x;
+            return T;
+        }
+        static double bai4(int n)
+        {
+            double S = 0;
+            for (int i = 1; i <= n; i++)
+                S = S + (2 * i + 1);
+            return S;
+        }
+        static double bai5(int n)
+        {
+            double S = 0;
+            for (int i = 0; i <= n; i++)
+                S = S + (i * (i + 1) * (i + 2));
+            return S;
+        }
+        static double bai6(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                    Console.Write("{0}\t", i);
+            }
+            return n;
+        }
+        static double bai7(int x, int n)
+        {
+            double S = 0;
+            for (int i = 1; i <= n; i++)
+                S = S + Math.Pow(x, i);
+            return S;
+        }
+
+        static double bai8(int x, int n) //S=x^1/1! + x^2/2! + x^n/n!
+        {
+            double S = 0;
+            double factorial = 1;
+            for (int i = 1; i <= n; i++)
+                factorial *= i;
+            S = S + Math.Pow(x, n) / factorial;
+            return S;
+        }
+        static double bai9(int n) // liet ke all cac uoc so le cua so nguyen duong n
+        {
+            for (int i = 1; i <= n; i += 2)
+            {
+                if (n % i == 0) // kiem tra neu i la uoc so cua n
+                {
+                    Console.WriteLine("{0}\t", i);
+                }
+            }
+            return n;
+        }
+        static double bai10(int n) // liet ke all cac uoc so chan cua so nguyen duong n
+        {
+            for (int i = 2; i <= n / 2; i += 2)
+            {
+                if (n % i == 0) // kiem tra neu i la uoc so cua n
+                {
+                    Console.WriteLine("{0}\t", i);
+                }
+            }
+            return n;
+        }
+        static double bai11(int n)
+        {
+            double S = 0;
+            for (int i = 1; i <= n; i++)
+                S = S + Math.Sqrt(i + Math.Sqrt((i - 1) + Math.Sqrt(2 + Math.Sqrt(1))));
+            return S;
+        }
+        static int bai12(int a, int b) // tim uoc so chung lon nhat
+        {
+            int USCLN;
+            USCLN = Math.Min(a, b);
+            for (int i = 1; i <= USCLN; i++)
+            {
+                if (a % i == 0 && b % i == 0)
+                {
+                    USCLN = i;
+                }
+            }
+            return USCLN;
+
+        }    
+
+        static double bai13(int n, int count)
+        {
+
+            //int count = 0;
+            for (int i = 0; i <= n; i++)
+                n /= 10;
+            count++;
+
+            return count;
+        }
+        static void bai14(double a, double b) // giai phuong trinh ax+b=0
+        {
+            if (a == 0 )
+            {
+                if( b == 0)
+                {
+                    Console.WriteLine("Phuong trinh co vo so nghiem");
+                }
+                else
+                {
+                    Console.WriteLine("Phuong trinh vo so nghiem");
+                }
+            }
+            else
+            {
+                double x = -b / a;
+                Console.WriteLine("Phuong trinh co nghiem x= {0}  ",x);
+            }
+        }
+        static void bai15(double a, double b, double c)
+        {
+            double delta = b * b - 4 * a * c;
+            if(delta > 0 )
+            {
+                double x1 = (-b + Math.Sqrt(delta))/(2*a);
+                double x2 = (-b - Math.Sqrt(delta) )/ (2 * a);
+                Console.WriteLine("Phuong trinh co 2 nghiem: ");
+                Console.WriteLine("x1= {0} ", x1);
+                Console.WriteLine("x2= {0} ", x2);
+            }
+            else if ( delta == 0)
+            {
+                double x = -b / (2 * a);
+                Console.WriteLine("Phuong trinh co nghiem kep: ");
+                Console.WriteLine("x= {0}" ,x);
+            }
+            else
+            {
+                Console.WriteLine("Phuong trinh vo nghiem");
+            }
+        }
+        static void Main(string[] args)
+        {
+            //Console.Write("Nhap n = ");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Nhap x = ");
+            //int x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Nhap a = ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Nhap b = ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Nhap c = ");
+            //double c = Convert.ToDouble(Console.ReadLine());
+            int USCLN = bai12(a, b);
+            //Console.WriteLine("Tong S = {0}", bai8(n, x));
+            Console.WriteLine("Uoc so chung lon nhat cua {0) va {1} la: {2} ",a,b,USCLN);
+            Console.ReadKey();
+        }
+    }
+}
