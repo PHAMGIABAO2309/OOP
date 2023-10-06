8ba28fc49fad04cf88dcc21731e7f822697bb4a7
Gia Bao <hieutiti1992103@gmail.com>
2023-10-06 21:02:56 +0700

    abc

---
+++ cài phương thức trong c#/Phương thức Delegate/Phương thức Delegate/Phương thức Delegate/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Delegate là một đối tượng gắn liền với 1 phương thức
+// Mỗi kho Delegate được gọi thì phương thức gắn liền với nó được thực thi
+namespace viduDelegate
+{
+    // tao 2 delegate
+    delegate void NhapDelegate();
+    delegate int TinhChuViDelegate();
+    class HinhChuNhat
+    {
+        Int32 a, b;
+        public void Nhap()
+        {
+            Console.Write("Nhap chieu dai hinh chu nhat: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Nhap chieu rong hinh chu nhat: ");
+            b = Convert.ToInt32(Console.ReadLine());
+        }
+        public int ChuVi()
+        {
+            return (a + b) * 2;
+        }
+    }
+    class HinhVuong
+    {
+        Int32 a;
+        public void Nhap()
+        {
+            Console.Write("Nhap canh hinh vuong: ");
+            a = Convert.ToInt32(Console.ReadLine());
+        }
+        public int ChuVi()
+        {
+            return a * 4;
+        }
+    }
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            HinhChuNhat hcn = new HinhChuNhat();
+            HinhVuong hv = new HinhVuong();
+            NhapDelegate delnhap1 = new NhapDelegate(hcn.Nhap);
+            delnhap1();
+            TinhChuViDelegate deltinh1 = new TinhChuViDelegate(hcn.ChuVi);
+            Console.WriteLine("Chu Vi Hinh Chu Nhat: {0} ", deltinh1());
+            NhapDelegate delnhap2 = new NhapDelegate(hv.Nhap);
+            delnhap2();
+            TinhChuViDelegate deltinh2 = new TinhChuViDelegate(hv.ChuVi);
+            Console.WriteLine("Chu Vi Hinh Vuong: {0} ", deltinh2());
+            Console.ReadKey();
+        }
+    }
+}
--- cài phương thức trong c#/cài phương thức trong c#/Program.cs
+++ cài phương thức trong c#/cài phương thức trong c#/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+namespace cai_dat_phuong_thuc
+{
+    public class HocPhan
+    {
+        public String MaHP = "023092004";
+        public String GetMaHP()
+        {
+            return MaHP;
+        }
+        public void SetMaHP(String MaHP_moi)
+        {
+            MaHP = MaHP_moi;
+        }
+        public String TenHP = "Co So Du Lieu";
+        public String GetTenHP()
+        {
+            return TenHP;
+        }
+        public void SetTenHP(String TenHP_moi)
+        {
+            TenHP = TenHP_moi;
+        }
+        protected UInt32 SoTCHP = 4;
+        public UInt32 GetSoTCHP()
+        {
+            return SoTCHP;
+        }
+        public void SetSoTCHP(UInt32 SoTc_moi)
+        {
+            SoTCHP = SoTc_moi;
+        }
+
+        protected String MotaHP = "Rat la nhuc dau nha";
+        private UInt32 SoTietLT = 15;
+        private UInt32 SoTietTH = 30;
+        
+        
+        
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HocPhan hp = new HocPhan();
+            Console.WriteLine("MaHP = {0}; \n TenHP = {1}", hp.MaHP, hp.TenHP);
+            hp.SetMaHP("12345");
+            hp.SetTenHP("Lap Trinh Huong Doi Tuong");
+            Console.WriteLine("MaHP = {0}; \n TenHP = {1}", hp.MaHP, hp.TenHP);
+            Console.ReadKey();
+        }
+    }
+}
