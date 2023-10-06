f4293d4a026be6d25b02209ef06ce1b4e4bcc66f
Gia Bao <hieutiti1992103@gmail.com>
2023-10-06 21:24:48 +0700

    abc

--- cài phương thức trong c#/Phương thức Anonymous/Phương thức Anonymous/Phương thức Anonymous/Program.cs
+++ cài phương thức trong c#/Phương thức Anonymous/Phương thức Anonymous/Phương thức Anonymous/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ViduAnonymous
+{
+    class Program
+    {
+        delegate int DelegateTong(int i, int j);
+        static void Main(string[] args)
+        {
+            int a = 10, b = 40;
+            DelegateTong delTong = delegate (int i, int j)
+            {
+                return i + j;
+            };
+            Console.WriteLine("Tong cua {0} va {1} la {2} ", a, b, delTong(a, b));
+            Console.ReadKey();
+        }
+    }
+}
--- cài phương thức trong c#/Phương thức Delegate/Phương thức Delegate/Phương thức Delegate/Program.cs
+++ cài phương thức trong c#/Phương thức Delegate/Phương thức Delegate/Phương thức Delegate/Program.cs
@@ -3,55 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//Delegate là một đối tượng gắn liền với 1 phương thức
-// Mỗi kho Delegate được gọi thì phương thức gắn liền với nó được thực thi
-namespace viduDelegate
+namespace ViduAnonymous
 {
-    // tao 2 delegate
-    delegate void NhapDelegate();
-    delegate int TinhChuViDelegate();
-    class HinhChuNhat
+    class Program
     {
-        Int32 a, b;
-        public void Nhap()
-        {
-            Console.Write("Nhap chieu dai hinh chu nhat: ");
-            a = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Nhap chieu rong hinh chu nhat: ");
-            b = Convert.ToInt32(Console.ReadLine());
-        }
-        public int ChuVi()
-        {
-            return (a + b) * 2;
-        }
-    }
-    class HinhVuong
-    {
-        Int32 a;
-        public void Nhap()
-        {
-            Console.Write("Nhap canh hinh vuong: ");
-            a = Convert.ToInt32(Console.ReadLine());
-        }
-        public int ChuVi()
-        {
-            return a * 4;
-        }
-    }
-    class Program 
-    {
+        delegate int DelegateTong(int i, int j);
         static void Main(string[] args)
         {
-            HinhChuNhat hcn = new HinhChuNhat();
-            HinhVuong hv = new HinhVuong();
-            NhapDelegate delnhap1 = new NhapDelegate(hcn.Nhap);
-            delnhap1();
-            TinhChuViDelegate deltinh1 = new TinhChuViDelegate(hcn.ChuVi);
-            Console.WriteLine("Chu Vi Hinh Chu Nhat: {0} ", deltinh1());
-            NhapDelegate delnhap2 = new NhapDelegate(hv.Nhap);
-            delnhap2();
-            TinhChuViDelegate deltinh2 = new TinhChuViDelegate(hv.ChuVi);
-            Console.WriteLine("Chu Vi Hinh Vuong: {0} ", deltinh2());
+            int a = 10, b = 40;
+            DelegateTong delTong = delegate (int i, int j)
+            {
+                return i + j;
+            };
+            Console.WriteLine("Tong cua {0} va {1} la {2} ", a, b, delTong(a, b));
             Console.ReadKey();
         }
     }
