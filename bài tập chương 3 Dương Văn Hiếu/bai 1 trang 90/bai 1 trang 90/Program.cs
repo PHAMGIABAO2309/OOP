fb88f986adcbf418afeb6505e40df054a8f437e5
Gia Bao <hieutiti1992103@gmail.com>
2023-11-29 22:08:34 +0700

    abc

---
+++ bài tập chương 3 Dương Văn Hiếu/bai 1 trang 90/bai 1 trang 90/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace bai1
+{
+    class NHANVIEN
+    {
+        string hoten;
+        string ngaysinh;
+        string diachi;
+        public virtual void Nhap()
+        {
+            Console.Write("\n Nhap ho va ten: ");
+            hoten = Console.ReadLine();
+            Console.Write("Nhap ngay sinh: ");
+            ngaysinh = Console.ReadLine();
+            Console.Write("Nhap dia chi: ");
+            diachi = Console.ReadLine();
+        }
+        public virtual void Xuat()
+        {
+            Console.Write(" {0} -  {1}  -  {2} ", hoten, ngaysinh, diachi);
+        }
+    }
+    class NVSANXUAT : NHANVIEN
+    {
+        int sosanpham;
+        public override void Nhap()
+        {
+            base.Nhap();
+            Console.Write("Nhap so san pham: ");
+            sosanpham = Convert.ToInt32(Console.ReadLine());
+            
+        }
+        public override void Xuat()
+        {
+            base.Xuat();
+            Console.WriteLine(" - {0}d ", sosanpham * 2000);
+        }
+    }
+    class DSNVSX
+    {
+        NVSANXUAT[] DS;
+        public void NhapDS()
+        {
+            int n;
+            Console.WriteLine("Nhap so luong nhan vien san xuat: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            DS = new NVSANXUAT[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("\n Nhap nhan vien bien che thu {0} :", i + 1);
+                DS[i] = new NVSANXUAT();
+                DS[i].Nhap();
+            }
+            Console.WriteLine("\n Thong tin nhan vien san xuat vua nhap: ");
+            for (int i=0; i<n; i++)
+            {
+
+                DS[i].Xuat();
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DSNVSX ds1 = new DSNVSX();
+            
+            ds1.NhapDS();
+            
+        }
+    }
+}
---
+++ bài tập chương 3 Dương Văn Hiếu/ví dụ trang 88/ví dụ trang 88/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace vidutrang80
+{
+    class NhanVien
+    {
+        string hoten;
+        string soCMND;
+        string phong;
+        double Luong;
+        public virtual void Nhap()
+        {
+            Console.Write("\nNhap ho va ten nhan vien: ");
+            hoten = Console.ReadLine();
+            Console.Write("Nhap so CMND: ");
+            soCMND = Console.ReadLine();
+            Console.Write("Nhap Phong: ");
+            phong = Console.ReadLine();
+            Console.Write("Nhap so tien luong: ");
+            Luong = Convert.ToDouble(Console.ReadLine());
+        }
+        public virtual void Xuat()
+        {
+            Console.Write("{0} - {1} - {2} - {3} ", hoten, soCMND, phong, Luong);
+        }
+    }
+    class NhanvienBienChe : NhanVien
+    {
+        int BacLuong;
+        public override void Nhap()
+        {
+            base.Nhap();
+            Console.WriteLine(" - {0} ", BacLuong);
+        }
+    }
+    class NhanVienHopDong : NhanVien
+    {
+        int sogio;
+        int heso;
+        public override void Nhap()
+        {
+            base.Nhap();
+            Console.Write("Nhap so gio: ");
+            sogio = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Nhap he so: ");
+            heso = Convert.ToInt32(Console.ReadLine());
+        }
+        public override void Xuat()
+        {
+            base.Xuat();
+            Console.WriteLine(" - {0} - {1} ", sogio, heso);
+        }
+    }
+    class DSNVBC
+    {
+        NhanvienBienChe[] DS;
+        public void NhapDSNVBC()
+        {
+            int n;
+            Console.WriteLine("Nhap so luong nhan vien bien ches: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            DS = new NhanvienBienChe[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("\n Nhap nhan vien bien che thu {0} :", i+1);
+                DS[i] = new NhanvienBienChe();
+                DS[i].Nhap();
+                DS[i].Xuat();
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DSNVBC nvbc = new DSNVBC();
+            nvbc.NhapDSNVBC();
+            
+            
+            Console.ReadKey();
+        }
+    }
+}
